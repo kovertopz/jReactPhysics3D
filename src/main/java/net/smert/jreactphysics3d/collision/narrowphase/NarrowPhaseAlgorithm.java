3ea306d43c97adfa260d38509597bbb4d06c939c
Jason Sorensen <git@smert.net>
2014-07-19 14:11:48 -0400

    Finished converting NarrowPhaseAlgorithm.cpp/NarrowPhaseAlgorithm.h and SphereVsSphereAlgorithm.cpp/SphereVsSphereAlgorithm.h.

--- src/main/java/net/smert/jreactphysics3d/collision/narrowphase/NarrowPhaseAlgorithm.java
+++ src/main/java/net/smert/jreactphysics3d/collision/narrowphase/NarrowPhaseAlgorithm.java
@@ -1,9 +1,50 @@
 package net.smert.jreactphysics3d.collision.narrowphase;
 
+import net.smert.jreactphysics3d.collision.BroadPhasePair;
+import net.smert.jreactphysics3d.collision.shapes.CollisionShape;
+import net.smert.jreactphysics3d.mathematics.Transform;
+import net.smert.jreactphysics3d.memory.MemoryAllocator;
+
 /**
+ * This class is an abstract class that represents an algorithm used to perform
+ * the narrow-phase of a collision detection. The goal of the narrow phase
+ * algorithm is to compute contact informations of a collision between two
+ * bodies.
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
 public class NarrowPhaseAlgorithm {
+
+    /// Reference to the memory allocator
+    protected MemoryAllocator mMemoryAllocator;
+
+    /// Overlapping pair of the bodies currently tested for collision
+    protected BroadPhasePair mCurrentOverlappingPair;
+
+    /// Private copy-constructor
+    protected NarrowPhaseAlgorithm(NarrowPhaseAlgorithm algorithm) {
+    }
+
+    /// Private assignment operator
+    protected NarrowPhaseAlgorithm operatorEqual(NarrowPhaseAlgorithm algorithm) {
+        return this;
+    }
+
+    // Constructor
+    public NarrowPhaseAlgorithm(MemoryAllocator memoryAllocator) {
+        mMemoryAllocator = memoryAllocator;
+        mCurrentOverlappingPair = null;
+    }
+
+    // Set the current overlapping pair of bodies
+    public void setCurrentOverlappingPair(BroadPhasePair overlappingPair) {
+        mCurrentOverlappingPair = overlappingPair;
+    }
+
+    /// Return true and compute a contact info if the two bounding volume collide
+    public abstract boolean testCollision(
+            CollisionShape collisionShape1, Transform transform1,
+            CollisionShape collisionShape2, Transform transform2,
+            ContactPointInfo contactInfo);
 
 }
--- src/main/java/net/smert/jreactphysics3d/collision/narrowphase/SphereVsSphereAlgorithm.java
+++ src/main/java/net/smert/jreactphysics3d/collision/narrowphase/SphereVsSphereAlgorithm.java
@@ -1,9 +1,69 @@
 package net.smert.jreactphysics3d.collision.narrowphase;
 
+import net.smert.jreactphysics3d.collision.shapes.CollisionShape;
+import net.smert.jreactphysics3d.collision.shapes.SphereShape;
+import net.smert.jreactphysics3d.mathematics.Transform;
+import net.smert.jreactphysics3d.mathematics.Vector3;
+import net.smert.jreactphysics3d.memory.MemoryAllocator;
+
 /**
+ * This class is used to compute the narrow-phase collision detection between
+ * two sphere collision shapes.
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
-public class SphereVsSphereAlgorithm {
+public class SphereVsSphereAlgorithm extends NarrowPhaseAlgorithm {
+
+    /// Private copy-constructor
+    protected SphereVsSphereAlgorithm(SphereVsSphereAlgorithm algorithm) {
+    }
+
+    /// Private assignment operator
+    protected SphereVsSphereAlgorithm operatorEqual(SphereVsSphereAlgorithm algorithm) {
+        return this;
+    }
+
+    // Constructor
+    public SphereVsSphereAlgorithm(MemoryAllocator memoryAllocator) {
+        super(memoryAllocator);
+    }
+
+    @Override
+    public boolean testCollision(
+            CollisionShape collisionShape1, Transform transform1,
+            CollisionShape collisionShape2, Transform transform2,
+            ContactPointInfo contactInfo) {
+
+        // Get the sphere collision shapes
+        SphereShape sphereShape1 = (SphereShape) collisionShape1;
+        SphereShape sphereShape2 = (SphereShape) collisionShape2;
+
+        // Compute the distance between the centers
+        Vector3 vectorBetweenCenters = transform2.getPosition() - transform1.getPosition();
+        float squaredDistanceBetweenCenters = vectorBetweenCenters.lengthSquare();
+
+        // Compute the sum of the radius
+        float sumRadius = sphereShape1.getRadius() + sphereShape2.getRadius();
+
+        // If the sphere collision shapes intersect
+        if (squaredDistanceBetweenCenters <= sumRadius * sumRadius) {
+            Vector3 centerSphere2InBody1LocalSpace = transform1.getInverse() * transform2.getPosition();
+            Vector3 centerSphere1InBody2LocalSpace = transform2.getInverse() * transform1.getPosition();
+            Vector3 intersectionOnBody1 = sphereShape1.getRadius()
+                    * centerSphere2InBody1LocalSpace.getUnit();
+            Vector3 intersectionOnBody2 = sphereShape2.getRadius()
+                    * centerSphere1InBody2LocalSpace.getUnit();
+            float penetrationDepth = sumRadius - (float) Math.sqrt(squaredDistanceBetweenCenters);
+
+            // Create the contact info object
+            contactInfo = new ContactPointInfo(
+                    vectorBetweenCenters.getUnit(), penetrationDepth,
+                    intersectionOnBody1, intersectionOnBody2);
+
+            return true;
+        }
+
+        return false;
+    }
 
 }
