736a344e8cdf5ebe0d9c633551df012a72c2b5e7
Jason Sorensen <git@smert.net>
2014-07-19 13:59:00 -0400

    Finished converting BroadPhaseAlgorithm.cpp/BroadPhaseAlgorithm.h and NoBroadPhaseAlgorithm.cpp/NoBroadPhaseAlgorithm.h.

--- src/main/java/net/smert/jreactphysics3d/collision/broadphase/BroadPhaseAlgorithm.java
+++ src/main/java/net/smert/jreactphysics3d/collision/broadphase/BroadPhaseAlgorithm.java
@@ -1,9 +1,50 @@
 package net.smert.jreactphysics3d.collision.broadphase;
 
+import net.smert.jreactphysics3d.collision.CollisionDetection;
+
 /**
+ * This class is an abstract class that represents an algorithm used to perform
+ * the broad-phase of a collision detection. The goal of the broad-phase
+ * algorithm is to compute the pair of bodies that can collide. But it's
+ * important to understand that the broad-phase doesn't compute only body pairs
+ * that can collide but could also pairs of body that doesn't collide but are
+ * very close. The goal of the broad-phase is to remove pairs of body that
+ * cannot collide in order to avoid to much bodies to be tested in the
+ * narrow-phase.
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
-public class BroadPhaseAlgorithm {
+public abstract class BroadPhaseAlgorithm {
+
+    /// Pair manager containing the overlapping pairs
+    protected PairManager mPairManager;
+
+    /// Reference to the collision detection object
+    protected CollisionDetection mCollisionDetection;
+
+    /// Private copy-constructor
+    protected BroadPhaseAlgorithm(BroadPhaseAlgorithm algorithm) {
+    }
+
+    /// Private assignment operator
+    protected BroadPhaseAlgorithm operatorEqual(BroadPhaseAlgorithm algorithm) {
+        return this;
+    }
+
+    // Constructor
+    public BroadPhaseAlgorithm(CollisionDetection collisionDetection) {
+        mPairManager = collisionDetection;
+        mCollisionDetection = collisionDetection;
+    }
+
+    // Return a pointer to the first active pair (used to iterate over the overlapping pairs)
+    public BodyPair beginOverlappingPairsPointer() {
+        return mPairManager.beginOverlappingPairsPointer();
+    }
+
+    // Return a pointer to the last active pair (used to iterate over the overlapping pairs)
+    public BodyPair endOverlappingPairsPointer() {
+        return mPairManager.endOverlappingPairsPointer();
+    }
 
 }
--- src/main/java/net/smert/jreactphysics3d/collision/broadphase/NoBroadPhaseAlgorithm.java
+++ src/main/java/net/smert/jreactphysics3d/collision/broadphase/NoBroadPhaseAlgorithm.java
@@ -1,9 +1,70 @@
 package net.smert.jreactphysics3d.collision.broadphase;
 
+import java.util.Set;
+import net.smert.jreactphysics3d.body.CollisionBody;
+import net.smert.jreactphysics3d.collision.CollisionDetection;
+import net.smert.jreactphysics3d.collision.shapes.AABB;
+
 /**
+ * This class implements a broad-phase algorithm that does nothing. It should be
+ * use if we don't want to perform a broad-phase for the collision detection.
  *
  * @author Jason Sorensen <sorensenj@smert.net>
  */
-public class NoBroadPhaseAlgorithm {
+public class NoBroadPhaseAlgorithm extends BroadPhaseAlgorithm {
+
+    /// All bodies of the world
+    protected Set<CollisionBody> mBodies;
+
+    /// Private copy-constructor
+    protected NoBroadPhaseAlgorithm(NoBroadPhaseAlgorithm algorithm) {
+    }
+
+    /// Private assignment operator
+    protected NoBroadPhaseAlgorithm operatorEqual(NoBroadPhaseAlgorithm algorithm) {
+        return this;
+    }
+
+    // Constructor
+    public NoBroadPhaseAlgorithm(CollisionDetection collisionDetection) {
+        super(collisionDetection);
+    }
+
+    // Notify the broad-phase about a new object in the world
+    public void addObject(CollisionBody body, AABB aabb) {
+
+        // For each body that is already in the world
+        for (CollisionBody it : mBodies) {
+
+            // Add an overlapping pair with the new body
+            mPairManager.addPair(it, body);
+        }
+
+        // Add the new body into the list of bodies
+        mBodies.add(body);
+    }
+
+    // Notify the broad-phase about an object that has been removed from the world
+    public void removeObject(CollisionBody body) {
+
+        // For each body that is in the world
+        for (CollisionBody it : mBodies) {
+
+            if (it.getID() != body.getID()) {
+
+                // Remove the overlapping pair with the new body
+                mPairManager.removePair(it.getID(), body.getID());
+            }
+        }
+
+        // Remove the body from the broad-phase
+        mBodies.remove(body);
+    }
+
+    // Notify the broad-phase that the AABB of an object has changed
+    public void updateObject(CollisionBody body, AABB aabb) {
+        // Do nothing
+        return;
+    }
 
 }
