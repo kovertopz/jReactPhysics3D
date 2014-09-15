836a40e2668f161c661aa63c7aff0b49731282e4
Jason Sorensen <git@smert.net>
2014-09-15 03:30:57 -0400

    Spelling error.

--- src/main/java/net/smert/jreactphysics3d/configuration/ContactsPositionCorrectionTechnique.java
+++ src/main/java/net/smert/jreactphysics3d/configuration/ContactsPositionCorrectionTechnique.java
@@ -3,7 +3,7 @@ package net.smert.jreactphysics3d.configuration;
 /**
  * Position correction technique used in the contact solver (for contacts)
  *
- * BAUMGARTE_CONTACTS : Faster but can be innacurate and can lead to unexpected bounciness in some situations (due to
+ * BAUMGARTE_CONTACTS : Faster but can be inaccurate and can lead to unexpected bounciness in some situations (due to
  * error correction factor being added to the bodies momentum).
  *
  * SPLIT_IMPULSES : A bit slower but the error correction factor is not added to the bodies momentum. This is the option
