4841ff60f34ef3c06c572d1cc4850461c572d9d2
Jason Sorensen <git@smert.net>
2014-09-15 03:27:12 -0400

    Spelling error.

--- src/main/java/net/smert/jreactphysics3d/configuration/JointsPositionCorrectionTechnique.java
+++ src/main/java/net/smert/jreactphysics3d/configuration/JointsPositionCorrectionTechnique.java
@@ -3,7 +3,7 @@ package net.smert.jreactphysics3d.configuration;
 /**
  * Position correction technique used in the constraint solver (for joints).
  *
- * BAUMGARTE_JOINTS : Faster but can be innacurate in some situations.
+ * BAUMGARTE_JOINTS : Faster but can be inaccurate in some situations.
  *
  * NON_LINEAR_GAUSS_SEIDEL : Slower but more precise. This is the option used by default.
  *
