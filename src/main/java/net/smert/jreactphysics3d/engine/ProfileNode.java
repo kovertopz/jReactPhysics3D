978db654185b0f807f285e70158e85ff4c1ea824
Jason Sorensen <git@smert.net>
2014-09-15 04:13:24 -0400

    Clean up engine/ProfileNode.java

--- src/main/java/net/smert/jreactphysics3d/engine/ProfileNode.java
+++ src/main/java/net/smert/jreactphysics3d/engine/ProfileNode.java
@@ -7,149 +7,149 @@ package net.smert.jreactphysics3d.engine;
  */
 public class ProfileNode {
 
-    // Name of the node
-    private String mName;
-
-    // Total number of calls of this node
-    private int mNbTotalCalls;
-
     // Starting time of the sampling of corresponding block of code
-    private float mStartingTime;
+    private float startingTime;
 
     // Total time spent in the block of code
-    private float mTotalTime;
+    private float totalTime;
+
+    // Total number of calls of this node
+    private int numTotalCalls;
 
     // Recursion counter
-    private int mRecursionCounter;
-
-    // Pointer to the parent node
-    private ProfileNode mParentNode;
+    private int recursionCounter;
 
     // Pointer to a child node
-    private ProfileNode mChildNode;
+    private ProfileNode childNode;
+
+    // Pointer to the parent node
+    private final ProfileNode parentNode;
 
     // Pointer to a sibling node
-    private ProfileNode mSiblingNode;
+    private ProfileNode siblingNode;
+
+    // Name of the node
+    private final String name;
 
     // Constructor
     public ProfileNode(String name, ProfileNode parentNode) {
-        mName = name;
-        mNbTotalCalls = 0;
-        mStartingTime = 0l;
-        mTotalTime = 0l;
-        mRecursionCounter = 0;
-        mParentNode = parentNode;
-        mChildNode = null;
-        mSiblingNode = null;
-
+        assert (name != null);
+        startingTime = 0l;
+        totalTime = 0l;
+        numTotalCalls = 0;
+        recursionCounter = 0;
+        childNode = null;
+        this.parentNode = parentNode;
+        siblingNode = null;
+        this.name = name;
         reset();
     }
 
-    // Return a pointer to the parent node
-    public ProfileNode getParentNode() {
-        return mParentNode;
+    // Destroy the node
+    public void destroy() {
+        //delete mChildNode;
+        childNode = null;
+        //delete mSiblingNode;
+        siblingNode = null;
     }
 
-    // Return a pointer to a sibling node
-    public ProfileNode getSiblingNode() {
-        return mSiblingNode;
+    // Called when we enter the block of code corresponding to this profile node
+    public void enterBlockOfCode() {
+        numTotalCalls++;
+
+        // If the current code is not called recursively
+        if (recursionCounter == 0) {
+
+            // Get the current system time to initialize the starting time of
+            // the profiling of the current block of code
+            startingTime = Timer.GetCurrentSystemTime() * 1000.0f;
+        }
+
+        recursionCounter++;
     }
 
-    // Return a pointer to a child node
-    public ProfileNode getChildNode() {
-        return mChildNode;
-    }
+    // Called when we exit the block of code corresponding to this profile node
+    public boolean exitBlockOfCode() {
+        recursionCounter--;
 
-    // Return the name of the node
-    public String getName() {
-        return mName;
-    }
+        if (recursionCounter == 0 && numTotalCalls != 0) {
 
-    // Return the total number of call of the corresponding block of code
-    public int getNumTotalCalls() {
-        return mNbTotalCalls;
-    }
+            // Get the current system time
+            float currentTime = Timer.GetCurrentSystemTime() * 1000.0f;
 
-    // Return the total time spent in the block of code
-    public float getTotalTime() {
-        return mTotalTime;
+            // Increase the total elasped time in the current block of code
+            totalTime += currentTime - startingTime;
+        }
+
+        // Return true if the current code is not recursing
+        return (recursionCounter == 0);
     }
 
     // Return a pointer to a sub node with a given name
     public ProfileNode findSubNode(String name) {
 
         // Try to find the node among the child nodes
-        ProfileNode child = mChildNode;
+        ProfileNode child = childNode;
         while (child != null) {
-            if (child.mName == name) {
+            if (child.name.equals(name)) {
                 return child;
             }
-            child = child.mSiblingNode;
+            child = child.siblingNode;
         }
 
         // The nose has not been found. Therefore, we create it
         // and add it to the profiler tree
         ProfileNode newNode = new ProfileNode(name, this);
-        newNode.mSiblingNode = mChildNode;
-        mChildNode = newNode;
+        newNode.siblingNode = childNode;
+        childNode = newNode;
 
         return newNode;
     }
 
-    // Called when we enter the block of code corresponding to this profile node
-    public void enterBlockOfCode() {
-        mNbTotalCalls++;
-
-        // If the current code is not called recursively
-        if (mRecursionCounter == 0) {
-
-            // Get the current system time to initialize the starting time of
-            // the profiling of the current block of code
-            mStartingTime = Timer.GetCurrentSystemTime() * 1000.0f;
-        }
-
-        mRecursionCounter++;
+    // Return the total time spent in the block of code
+    public float getTotalTime() {
+        return totalTime;
     }
 
-    // Called when we exit the block of code corresponding to this profile node
-    public boolean exitBlockOfCode() {
-        mRecursionCounter--;
+    // Return the total number of call of the corresponding block of code
+    public int getNumTotalCalls() {
+        return numTotalCalls;
+    }
 
-        if (mRecursionCounter == 0 && mNbTotalCalls != 0) {
+    // Return a pointer to a child node
+    public ProfileNode getChildNode() {
+        return childNode;
+    }
 
-            // Get the current system time
-            float currentTime = Timer.GetCurrentSystemTime() * 1000.0f;
+    // Return a pointer to the parent node
+    public ProfileNode getParentNode() {
+        return parentNode;
+    }
 
-            // Increase the total elasped time in the current block of code
-            mTotalTime += currentTime - mStartingTime;
-        }
+    // Return a pointer to a sibling node
+    public ProfileNode getSiblingNode() {
+        return siblingNode;
+    }
 
-        // Return true if the current code is not recursing
-        return (mRecursionCounter == 0);
+    // Return the name of the node
+    public String getName() {
+        return name;
     }
 
     // Reset the profiling of the node
-    public void reset() {
-        mNbTotalCalls = 0;
-        mTotalTime = 0l;
+    public final void reset() {
+        totalTime = 0;
+        numTotalCalls = 0;
 
         // Reset the child node
-        if (mChildNode != null) {
-            mChildNode.reset();
+        if (childNode != null) {
+            childNode.reset();
         }
 
         // Reset the sibling node
-        if (mSiblingNode != null) {
-            mSiblingNode.reset();
+        if (siblingNode != null) {
+            siblingNode.reset();
         }
-    }
-
-    // Destroy the node
-    public void destroy() {
-        //delete mChildNode;
-        mChildNode = null;
-        //delete mSiblingNode;
-        mSiblingNode = null;
     }
 
 }
