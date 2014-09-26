2064d1a17a33d9472acf2ab07b2571c53cadba63
Jason Sorensen <git@smert.net>
2014-09-26 09:26:03 -0400

    Realized the jarClassLoader from jdotsoft.com is a GNU General Public License, version 2 (Author claims GNU General Public License, version 3 but header says otherwise) and is incompatible with zLib. Technically it is compatible but would require releasing all code under the same license.

---
+++ examples/src/main/java/net/smert/jreactphysics3d/examples/JarClassLoader.java
@@ -0,0 +1,12 @@
+package net.smert.jreactphysics3d.examples;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class JarClassLoader extends ClassLoader {
+
+    public void invokeMain(String mainClass, String[] args) {
+    }
+
+}
